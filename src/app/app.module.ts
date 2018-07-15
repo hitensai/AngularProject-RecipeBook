25a97c264e27199c0bc4098a632be2a1c358878a
hiten sai <hitensai279@gmail.com>
2018-07-14 19:37:31 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { ShoppinglistEditComponent } from './shoppinglist/shoppinglist-edit/shoppinglist-edit.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { RecipeComponent } from './recipies/recipe/recipe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    ShoppinglistComponent,
+    ShoppinglistEditComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
